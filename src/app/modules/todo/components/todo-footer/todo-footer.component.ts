9027c72bbbc9d0e69d49dfade76daf10392fd561
Jerico De Guzman <jjddeguzman@gmail.com>
2025-05-12 18:11:45 +0800

    update footer to be dynamic

--- src/app/modules/todo/components/todo-footer/todo-footer.component.ts
+++ src/app/modules/todo/components/todo-footer/todo-footer.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-footer',
   standalone: true,
   imports: [],
   templateUrl: './todo-footer.component.html',
-  styleUrl: './todo-footer.component.scss'
+  styleUrl: './todo-footer.component.scss',
 })
 export class TodoFooterComponent {
-
+  @Input() itemCount: number;
 }
