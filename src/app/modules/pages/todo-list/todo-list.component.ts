52d4fdd94f1ad491d468ef67914080467a44bec3
Jerico De Guzman <jjddeguzman@gmail.com>
2025-05-12 15:42:13 +0800

    make todo input as a dumb card

--- src/app/modules/pages/todo-list/todo-list.component.ts
+++ src/app/modules/pages/todo-list/todo-list.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { TodoFooterComponent } from '../../todo/components/todo-footer/todo-footer.component';
 import { TodoHeaderComponent } from '../../todo/components/todo-header/todo-header.component';
 import { TodoInputComponent } from '../../todo/components/todo-input/todo-input.component';
@@ -7,10 +13,36 @@ import { TodoItemComponent } from '../../todo/components/todo-item/todo-item.com
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [TodoHeaderComponent, TodoInputComponent, TodoItemComponent, TodoFooterComponent],
+  imports: [
+    TodoHeaderComponent,
+    TodoInputComponent,
+    TodoItemComponent,
+    TodoFooterComponent,
+  ],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.scss'
+  styleUrl: './todo-list.component.scss',
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  inputForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.inputForm = this.formBuilder.group({
+      whatNeedsToBeDone: [null, Validators.required],
+    });
+  }
+
+  get whatNeedsToBeDone(): AbstractControl {
+    return this.inputForm?.get('whatNeedsToBeDone');
+  }
+
+  onAdd(): void {
+    if (this.inputForm.valid) {
+      console.log(this.whatNeedsToBeDone.value);
+      this.inputForm.reset();
+    } else {
+      alert('Add a todo');
+    }
+  }
 }
--- src/app/modules/todo/components/todo-input/todo-input.component.ts
+++ src/app/modules/todo/components/todo-input/todo-input.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-todo-input',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './todo-input.component.html',
-  styleUrl: './todo-input.component.scss'
+  styleUrl: './todo-input.component.scss',
 })
 export class TodoInputComponent {
+  @Input() inputForm: FormGroup;
+  @Input() whatNeedsToBeDone: AbstractControl;
+  @Output() onAddClickEvent: EventEmitter<any> = new EventEmitter();
 
+  onAdd(): void {
+    this.onAddClickEvent.emit();
+  }
 }
