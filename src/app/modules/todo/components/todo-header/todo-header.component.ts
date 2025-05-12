8db3643379b8a71c5290464c23cd8924814faf61
Jerico De Guzman <jjddeguzman@gmail.com>
2025-05-12 14:29:05 +0800

    create input for todo header

--- src/app/modules/todo/components/todo-header/todo-header.component.ts
+++ src/app/modules/todo/components/todo-header/todo-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-header',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './todo-header.component.scss'
 })
 export class TodoHeaderComponent {
-
+@Input() headerTitle: string = '';
 }
